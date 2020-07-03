c35e81af6e4e18616fd5566681b7789d2b3fce8b
Maxwel <maxpdr96@gmail.com>
2020-07-03 20:42:41 -0300

    Configuring database H2 and POM

--- src/main/java/br/com/hidarisoft/ftdb/App.java
+++ src/main/java/br/com/hidarisoft/ftdb/App.java
@@ -1,3 +1,5 @@
+package br.com.hidarisoft.ftdb;
+
 public class App {
     public static void main(String[] args) {
         System.out.println("FTDB BATCH JAVA 6");
---
+++ src/main/java/br/com/hidarisoft/ftdb/configs/ImportLauncher.java
@@ -0,0 +1,8 @@
+package br.com.hidarisoft.ftdb.configs;
+
+import java.text.SimpleDateFormat;
+
+public class ImportLauncher {
+    SimpleDateFormat sdf = null;
+
+}
